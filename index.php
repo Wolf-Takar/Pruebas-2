46101a0bb14a8261b4b4d747469305304046280a
Javier <hernandezmartinezjavier115@gmail.com>
2020-10-14 14:32:09 -0500

    Merge

--- index.php
+++ index.php
@@ -6,10 +6,6 @@
     <title>Document</title>
 </head>
 <body>
-<<<<<<< HEAD
     <p>Esto esta editado desde otro usuario</p>
-=======
-    <p>Esto lo edite como colaborador</p>
->>>>>>> f7ef09a026b06b2c92a1a370b74ef1915b2885da
 </body>
 </html>
